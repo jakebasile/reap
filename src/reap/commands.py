cafa85f43ebe31f0acbfb87d38254533639b7603
Jake Basile <jakebasile@me.com>
2012-02-19 21:12:45 -0600

    The Huge Commit. I got involved and forgot to make progress checkins.
    * Rewrote the entire backend.
    * Shifted focus from tasks to entries.
    * Added create command.
    * Added list command.
    * Changed how Status prints data.
    * Changed login params. Shouldn't break against existing logins.
    * Removed bookmarks until I can figure out if they work with new Entries focus.

--- src/reap/commands.py
+++ src/reap/commands.py
@@ -12,27 +12,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from reap.support import *
-from reap.constants import *
-
 import reap.api
-import reap.support
-import urllib2
 import keyring
+import os.path
 
-from json import loads, dumps
-from base64 import b64encode
-from urllib2 import Request, urlopen
-from keyring import set_password, get_password
-from os.path import expanduser, exists
-from pickle import dump, load
-from re import compile, IGNORECASE
-from getpass import getpass
+# Constants
+
+STATUS_TASK_FORMAT = '''Project:    {entry.project_name}
+Task:       {entry.task_name}
+ID:         {entry.id}
+Notes:      {entry.notes}
+Time:       {hours}:{minutes:02d}
+'''
+
+# Support Functions
+
+def save_info(base_uri, username):
+    with open(os.path.expanduser('~/.reaprc'), 'w') as file:
+        file.write(base_uri + '\n')
+        file.write(username + '\n')
+
+def load_info():
+    if os.path.exists(os.path.expanduser('~/.reaprc')):
+        with open(os.path.expanduser('~/.reaprc'), 'r') as file:
+            base_uri = file.readline().strip()
+            username = file.readline().strip()
+            return (base_uri, username)
+    else:
+        print 'Please login first.'
+
+# def save_bookmarks(bookmarks):
+#     with open(expanduser('~/.reapbkmrks'), 'w') as file:
+#         dump(bookmarks, file)
+#
+# def load_bookmarks():
+#     path = expanduser('~/.reapbkmrks')
+#     if exists(path):
+#         with open(path, 'r') as file:
+#             return load(file)
+#     else:
+#         return {}
+
+def get_timesheet():
+    info = load_info()
+    if info:
+        base_uri = info[0]
+        username = info[1]
+        passwd = keyring.get_password(base_uri, username)
+        return reap.api.Timesheet(base_uri, username, passwd)
+
+# Commands
 
 def login(args):
     password = getpass()
     try:
-        print args.baseuri
         ts = reap.api.Timesheet(args.baseuri, args.username, password)
     except ValueError:
         print 'Invalid Credentials.'
@@ -41,103 +74,138 @@ def login(args):
         print 'Unable to communicate. Check information and try again.'
         return
     keyring.set_password(args.baseuri, args.username, password)
-    reap.support.save_info(args.baseuri, args.username)
+    save_info(args.baseuri, args.username)
     print 'You are now logged in.'
 
 def status(args):
-    ts = reap.support.get_timesheet()
-    total = 0
-    running_entry = None
-    stopped_entries = []
-    for entry in ts.entries():
-        if entry.started:
-            running_entry = entry
-        else:
-            stopped_entries += [entry]
-        total += entry.hours
-    if running_entry:
-        print '# Currently Running Timer:'
-        print str.format(
-            STATUS_TASK_FORMAT,
-            entry = running_entry,
-            hours = int(running_entry.hours),
-            minutes = int(running_entry.hours % 1 * 60),
-        )
-    if len(stopped_entries) > 0:
-        print '# Stopped Entries:'
-        for entry in stopped_entries:
+    ts = get_timesheet()
+    if ts:
+        total = 0
+        running_entry = None
+        stopped_entries = []
+        for entry in ts.entries():
+            if entry.started:
+                running_entry = entry
+            else:
+                stopped_entries += [entry]
+            total += entry.hours
+        if running_entry:
+            print '# Currently Running Timer:'
             print str.format(
                 STATUS_TASK_FORMAT,
-                entry = entry,
-                hours = int(entry.hours),
-                minutes = int(entry.hours % 1 * 60),
+                entry = running_entry,
+                hours = int(running_entry.hours),
+                minutes = int(running_entry.hours % 1 * 60),
             )
+        if len(stopped_entries) > 0:
+            print '# Stopped Entries:'
+            for entry in stopped_entries:
+                print str.format(
+                    STATUS_TASK_FORMAT,
+                    entry = entry,
+                    hours = int(entry.hours),
+                    minutes = int(entry.hours % 1 * 60),
+                )
 
-def bookmark(args):
-    bookmarks = load_bookmarks()
-    bookmarks[args.name] = (args.projectid, args.taskid)
-    save_bookmarks(bookmarks)
-
-def bookmarks(args):
-    bookmarks = load_bookmarks()
-    print '\nCurrent Bookmarks:'
-    for key in bookmarks.keys():
-        bkmk = bookmarks[key]
-        print str.format('{}: P:[{}] T:[{}]', key, bkmk[0], bkmk[1])
-    print ''
-
-def note(args):
-    task_info = get_task_info(args.task)
-    entry = get_entry(task_info[0], task_info[1])
-    if args.append:
-        entry['notes'] = entry['notes'] + args.note
-    else:
-        entry['notes'] = args.note
-    request = get_request('daily/update/' + str(entry['id']))
-    request.add_data(dumps(entry))
-    response = urlopen(request)
-    if response.code == 200:
-        print 'Success!'
+# def bookmark(args):
+#     ts = get_timesheet()
+#     if ts:
+#         bookmark_entry = None
+#         for entry in ts.entries():
+#             if entry.id == int(args.entryid):
+#                 bookmark_entry = entry
+#         if bookmark_entry:
+#             bookmarks = load_bookmarks()
+#             bookmarks[args.name] = (args.entryid, bookmark_entry.project_name, bookmark_entry.task_name)
+#             save_bookmarks(bookmarks)
+#             print 'Bookmark added.'
+#         else:
+#             print 'No such task on your timesheet.'
+#
+# def bookmarks(args):
+#     bookmarks = load_bookmarks()
+#     if len(bookmarks) > 0:
+#         for key in bookmarks.keys():
+#             bkmk = bookmarks[key]
+#             print str.format('{}: {bkmk[0]} ({bkmk[1]} - {bkmk[2]})', key, bkmk = bkmk)
 
 def start(args):
-    task_info = get_task_info(args.task)
-    entry = get_entry(task_info[0], task_info[1])
-    if not entry:
-        # entry not found, need to create it.
-        req = {
-            'notes': '',
-            'hours': '',
-            'project_id': task_info[0],
-            'task_id': task_info[1],
-        }
-        request = get_request('daily/add')
-        request.add_data(dumps(req))
-        response = urlopen(request)
-        if response.code == 200:
-            print 'Timer Started!'
-    else:
-        request = get_request('daily/timer/' + str(entry['id']))
-        response = urlopen(request)
-        if response.code == 200:
-            print 'Timer Started!'
+    ts = get_timesheet()
+    if ts:
+        found = None
+        id = int(args.entryid)
+        for entry in ts.entries():
+            if entry.id == id:
+                found = entry
+                break
+        if found:
+            if found.started:
+                print 'Entry timer already started.'
+            else:
+                entry.start()
+                print 'Entry timer started.'
+        else:
+            print 'No entry with that ID.'
 
 def stop(args):
-    active = get_active_timer()
-    if active:
-        request = get_request('daily/timer/' + str(active['id']))
-        response = urlopen(request)
-        if response.code == 200:
-            print 'Timer stopped!'
-    else:
-        print 'No active timer!'
+    ts = get_timesheet()
+    if ts:
+        found = None
+        for entry in ts.entries():
+            if entry.started:
+                found = entry
+                break
+        if found:
+            found.stop()
+            print 'Entry timer stopped.'
+        else:
+            print 'No timers to stop.'
+
+def list(args):
+    ts = get_timesheet()
+    if ts:
+        print '# Projects and Tasks:'
+        for proj in ts.projects():
+            print proj.name
+            for task in proj.tasks():
+                print str.format('|----{} ({} {})', task.name, proj.id, task.id)
+            print ''
+
+def create(args):
+    ts = get_timesheet()
+    if ts:
+        dec_time = 0.0
+        if args.time:
+            split = args.time.split(':')
+            hours = float(split[0])
+            minutes = float(split[1])
+            dec_time = hours + minutes / 60
+        notes = args.notes or ''
+        for proj in ts.projects():
+            if proj.id == int(args.projectid):
+                for task in proj.tasks():
+                    if task.id == int(args.taskid):
+                        entry = ts.entries().create(task, hours = dec_time, notes = notes)
+                        print '# Added entry:'
+                        print str.format(
+                            STATUS_TASK_FORMAT,
+                            entry = entry,
+                            hours = int(entry.hours),
+                            minutes = int(entry.hours % 1 * 60),
+                        )
+                        return
+        print 'No project/task found with those IDs.'
 
 def delete(args):
-    task_info = get_task_info(args.task)
-    entry = get_entry(task_info[0], task_info[1])
-    if not entry:
-        print 'No task found.'
-    else:
-        request = get_request('daily/delete/' + str(entry['id']))
-        response = urlopen(request)
-        if response:
-            print 'Task deleted'
+    ts = get_timesheet()
+    if ts:
+        found = None
+        for entry in ts.entries():
+            if entry.id == int(args.entryid):
+                found = entry
+                break
+        if found:
+            found.delete()
+            print 'Entry deleted.'
+        else:
+            print 'No entry with that ID.'
