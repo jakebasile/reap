fa7ccd2128fd194ec8d66834002136437797b3c9
Jake Basile <jakebasile@me.com>
2012-02-28 16:26:34 -0600

    Moved around a bunch of code to split up admin and timesheet actions. They looks similar but differ in many respects. It will be easier to maintain both the basic and admin interface separately.

--- src/reap/api/timesheet_tests.py
+++ src/reap/api/timesheet_tests.py
@@ -16,7 +16,7 @@ import unittest
 import random
 import string
 import datetime
-from reap.api import *
+from reap.api.timesheet import *
 
 def random_string(length = 5):
     return ''.join(random.choice(string.ascii_lowercase) for x in xrange(length))
@@ -178,93 +178,6 @@ class TestEntry(TimesheetTest):
         self.assertIsNotNone(new_entry)
         self.assertTrue(new_entry.started)
         entry.delete()
-
-class HarvestTest(unittest.TestCase):
-    def setUp(self):
-        # You need to have valid account info in an info.txt file to run tests.
-        with open('info.txt') as info:
-            self.base_uri = info.readline().strip()
-            self.username = info.readline().strip()
-            self.password = info.readline().strip()
-        self.hv = Harvest(self.base_uri, self.username, self.password)
-
-class TestHarvestLogin(HarvestTest):
-    def runTest(self):
-        hv = Harvest(self.base_uri, self.username, self.password)
-        self.assertIsNotNone(hv)
-        self.assertRaises(ValueError, Harvest, random_string(), random_string(), random_string())
-
-class TestPeople(HarvestTest):
-    def test_get(self):
-        people = self.hv.people()
-        self.assertIsNotNone(people)
-        for person in people:
-            self.assertIsNotNone(person)
-            self.assertIsNotNone(person.id)
-            self.assertIsNotNone(person.email)
-            self.assertIsNotNone(person.first_name)
-            self.assertIsNotNone(person.last_name)
-            self.assertIsNotNone(person.all_future)
-            self.assertIsNotNone(person.active)
-            self.assertIsNotNone(person.admin)
-            self.assertIsNotNone(person.contractor)
-            self.assertIsNotNone(person.telephone)
-            self.assertIsNotNone(person.timezone)
-            # optional ones
-            # self.assertIsNotNone(person.department)
-            # self.assertIsNotNone(person.default_rate)
-
-    def test_create(self):
-        fn = random_string()
-        ln = random_string()
-        email = random_string() + '@example.com'
-        contractor = bool(random.getrandbits(1))
-        admin = bool(random.getrandbits(1))
-        department = random_string()
-        default_rate = random.random() * 10
-        person = self.hv.people().create(
-            fn,
-            ln,
-            email,
-            contractor = contractor,
-            admin = admin,
-            department = department,
-            default_rate = default_rate,
-        )
-        self.assertIsNotNone(person)
-        self.assertIsNotNone(person)
-        self.assertIsNotNone(person.id)
-        self.assertIsNotNone(person.email)
-        self.assertEqual(person.email, email)
-        self.assertIsNotNone(person.first_name)
-        self.assertEqual(person.first_name, fn)
-        self.assertIsNotNone(person.last_name)
-        self.assertEqual(person.last_name, ln)
-        self.assertIsNotNone(person.all_future)
-        self.assertIsNotNone(person.active)
-        self.assertIsNotNone(person.admin)
-        self.assertEqual(person.admin, admin)
-        self.assertIsNotNone(person.contractor)
-        self.assertEqual(person.contractor, contractor)
-        self.assertIsNotNone(person.telephone)
-        self.assertIsNotNone(person.timezone)
-        # clean up
-        person.delete()
-
-    def test_delete(self):
-        person = self.hv.people().create(
-            random_string(),
-            random_string(),
-            random_string() + '@example.com',
-        )
-        self.assertIsNotNone(person)
-        id = person.id
-        person.delete()
-        # ensure it's no longer there.
-        for p in self.hv.people():
-            if p.id == id:
-                self.fail()
-
 
 
 if __name__ == '__main__':
