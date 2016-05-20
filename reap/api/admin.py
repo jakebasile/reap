0da2ee80371170559225935a21d388c4536489e3
Jake Basile <jake@jakebasile.com>
2016-05-20 00:20:21 -0500

    remove extra encode call

--- reap/api/admin.py
+++ reap/api/admin.py
@@ -219,7 +219,7 @@ class Client:
     '''A client in the Harvest system.'''
     def __init__(self, hv, json):
         self.hv = hv
-        self.name = json['name'].encode('utf-8')
+        self.name = json['name']
         self.id = json['id']
         self.created = parse_time(json['created_at'])
         self.updated = parse_time(json['updated_at'])
@@ -228,7 +228,7 @@ class Client:
         self.currency = json['currency']
         self.currency_symbol = json['currency_symbol']
         self.active = json['active']
-        self.details = json['details'].encode('utf-8')
+        self.details = json['details']
         timeframes = json['default_invoice_timeframe']
         if timeframes and timeframes != 'Custom':
             pst = lambda timestr: \
